b224d89e8d6ffbd630146d70a7faeec5df3034f5
LinXunFeng <598600855@qq.com>
2021-08-01 00:40:38 +0800

    数据类型-整数、浮点数

---
+++ 01_Dart基本语法/02_数据类型.dart
@@ -0,0 +1,11 @@
+main(List<String> args) {
+  // 整型
+  int age = 18;
+  int hexAge = 0x12;
+  print(age); // 18
+  print(hexAge); // 18
+
+  // 浮点类型 double
+  double height = 1.8;
+  print(height); // 1.8
+}
