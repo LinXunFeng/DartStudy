113810efed9fe7643e7570a2c59522ce8ba0c82c
LinXunFeng <598600855@qq.com>
2021-08-04 22:10:55 +0800

    Mixin混入

---
+++ 12_mixin混入.dart
@@ -0,0 +1,22 @@
+// mixin Runner {} // 可以不使用on，默认是 on Object
+
+mixin Runner on Animal {
+  run() {
+    print('跑起来了');
+  }
+}
+
+class Animal {}
+
+class SuperPeople extends Animal with Runner {}
+
+// 报错：'Runner' can't be mixed onto 'Object' because 'Object' doesn't implement 'Animal'.
+// class Person with Runner {}
+
+// 报错：Classes can only extend other classes.
+// class Person extends Runner {} // Runner无法被继承
+
+main(List<String> args) {
+  var p = SuperPeople();
+  p.run(); // 跑起来了
+}
